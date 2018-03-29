867016d62ec3d1f53551b0f58a568ec720bb3abe
sami-mai <samirah.maison@gmail>
2018-03-29 14:26:37 +0300

    test case to test if the object is initialized properly

--- user_test.py
+++ user_test.py
@@ -15,13 +15,24 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Sami-mai","@sami!mai62") # create contact object
+        self.new_user = User("Sami-mai","@samI!maI62") # create contact object
 
 
     def tearDown(self):
-
         '''
         tearDown method that does clean up after each test case has run.
         '''
 
         User.user_list = []
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Sami-mai")
+        self.assertEqual(self.new_user.password,"@samI!maI62")
+
+
+if __name__ == '__main__':
+    unittest.main()
