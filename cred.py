70a4bb63fa6af755063bd405fe27a221b76a1723
sami-mai <samirah.maison@gmail>
2018-04-01 07:14:47 +0300

    define class method that finds credentials by username

--- cred.py
+++ cred.py
@@ -25,3 +25,34 @@ class Cred:
         save_cred method saves cred objects into cred_list
         '''
         Cred.cred_list.append(self)
+
+    @classmethod
+    def cred_exists(cls,accountName):
+        '''
+        Method that checks if a user's credentials exist in the cred list.
+        Args:
+        accountName: account name to search if the credentials exist.
+        Returns :
+        Boolean: True or false depending if the user's credentials exist.
+        '''
+
+        for cred in cls.cred_list:
+            if cred.accountName == accountName:
+                return True
+
+        return False
+
+    @classmethod
+    def find_by_accountName(cls,accountName):
+        '''
+        Method that takes in an account name and returns the credentials that match that account name.
+
+        Args:
+            accountName: Account name to search for
+        Returns :
+            Full credentials that matches the accountName.
+        '''
+
+        for cred in cls.cred_list:
+            if cred.accountName == accountName:
+                return cred
