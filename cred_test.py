da874fc6699fb8d0f3562401518b1735efa8b0e9
sami-mai <samirah.maison@gmail>
2018-04-02 19:42:22 +0300

    test gen_password

--- cred_test.py
+++ cred_test.py
@@ -1,28 +1,23 @@
 import unittest
 from cred import Cred
-import pyperclip
 
 class TestCred(unittest.TestCase):
-
     '''
     Test class that defines test cases for the Cred class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
         self.new_cred = Cred("Facebook","sami.mai@gmail.com","Sami-maifb","@samI!maI4fb")
 
-
     def tearDown(self):
         '''
         tearDown method that does clean up after each test case has run.
         '''
-
         Cred.cred_list = []
 
     def test_init(self):
@@ -91,8 +86,6 @@ class TestCred(unittest.TestCase):
         test_cred.password = Cred.gen_password("Bart-Menz")
         test_cred.save_cred()
         self.assertTrue(len(test_cred.password) > 2)
-
-
 
 if __name__ == '__main__':
     unittest.main()
